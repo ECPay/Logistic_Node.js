96a077755288e27d615a8a242b30becd8555eac0
allen.chang <allennchang@gmail.com>
2018-09-14 10:48:22 +0800

    物流參數檢核調整GoodsName,SenderName,ReceiverName

--- ecpay_logistics_nodejs/example/create_b2c_c2c.js
+++ ecpay_logistics_nodejs/example/create_b2c_c2c.js
@@ -4,21 +4,21 @@
 const ecpay_logistics = require('../lib/ecpay_logistics.js');
 // 參數值為[PLEASE MODIFY]者，請在每次測試時給予獨特值
 let base_param = {
-	MerchantTradeNo:"", // 請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
+	MerchantTradeNo:"fyaabbsdbc93", // 請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
 	MerchantTradeDate:"2018/03/09 11:00:45", // 請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
 	LogisticsType:"CVS",
 	LogisticsSubType:"UNIMART",//UNIMART、FAMI、HILIFE、UNIMARTC2C、FAMIC2C、HILIFEC2C
 	GoodsAmount:"200",
-	CollectionAmount:"",
+	CollectionAmount:"200",
 	IsCollection:"",
 	GoodsName:"test",
 	SenderName:"綠界科技",
-	SenderPhone:"",
+	SenderPhone:"29788833",
 	SenderCellPhone:"0912345678",
 	ReceiverName:"綠界科技",
 	ReceiverPhone:"0229768888",
 	ReceiverCellPhone:"0912345678",
-	ReceiverEmail:"",
+	ReceiverEmail:"tesy@gmail.com",
 	TradeDesc:"",
 	ServerReplyURL:"http://192.168.0.1/ReceiverServerReply", // 物流狀況會通知到此URL
 	ClientReplyURL:"",
--- ecpay_logistics_nodejs/example/create_home.js
+++ ecpay_logistics_nodejs/example/create_home.js
@@ -4,18 +4,18 @@
 const ecpay_logistics = require('../lib/ecpay_logistics.js');
 // 參數值為[PLEASE MODIFY]者，請在每次測試時給予獨特值
 let base_param = {
-	MerchantTradeNo:"", // 請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
-	MerchantTradeDate:"", // 請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
+	MerchantTradeNo:"fd7e9faasdsabb72bc93", // 請帶20碼uid, ex: f0a0d7e9fae1bb72bc93, 為aiocheckout時所產生的
+	MerchantTradeDate:"2017/05/17 16:23:45", // 請帶交易時間, ex: 2017/05/17 16:23:45, 為aiocheckout時所產生的
 	LogisticsType:"Home",
 	LogisticsSubType:"TCAT",
 	GoodsAmount:"100",
-	CollectionAmount:"",
-	IsCollection:"",
-	GoodsName:"",
-	SenderName:"綠界科技",
+	CollectionAmount:"N",
+	IsCollection:"N",
+	GoodsName:"123435",
+	SenderName:"綠界",
 	SenderPhone:"",
 	SenderCellPhone:"0912345678",
-	ReceiverName:"綠界科技",
+	ReceiverName:"綠界!@#",
 	ReceiverPhone:"",
 	ReceiverCellPhone:"0912345678",
 	ReceiverEmail:"",
